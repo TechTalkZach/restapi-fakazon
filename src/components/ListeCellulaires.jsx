6ad7cc2cb617c44342e48634bfdebb59834fe273
zach <zachalvarezxvi@gmail.com>
2022-11-27 00:32:06 -0500

    ListeCellulaire Component finished, missing Alert

--- src/components/ListeCellulaires.jsx
+++ src/components/ListeCellulaires.jsx
@@ -1,8 +1,84 @@
 import React from 'react'
 
 const ListeCellulaires = () => {
+
+    var cellulaires = [
+        {
+            "id" : "1",
+            "nom" : "Samsung S22",
+            "categorie" : "Samsung",
+            "quantite" : "23",
+            "prix" : "800"
+
+        },
+        {
+            "id" : "2",
+            "nom" : "Samsung S22",
+            "categorie" : "Samsung",
+            "quantite" : "23",
+            "prix" : "800"
+
+        },
+        {
+            "id" : "3",
+            "nom" : "Samsung S22",
+            "categorie" : "Samsung",
+            "quantite" : "23",
+            "prix" : "800"
+
+        },
+
+    ]
+
+    // const [cellulaire, setCellulaire] = useState([]);
+
+
+
   return (
-    <div>ListeCellulaires</div>
+    <div>
+                 <h2 className="text-center">Liste des Cellulaires</h2>
+                 <div className = "row">
+                    {/* OnClick ajouterCellulaire() */}
+                    <button className="btn btn-primary"> Ajouter Cellulaire</button>
+                 </div>
+                 <br></br>
+                 <div className = "row">
+                        <table className = "table table-striped table-bordered">
+
+                            <thead>
+                                <tr>
+                                    <th> ID</th>
+                                    <th> Nom</th>
+                                    <th> Categorie</th>
+                                    <th> Qte</th>
+                                    <th>Prix</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    cellulaires.map(
+                                        cellulaire => 
+                                        <tr key = {cellulaire.id}>
+                                             <td> { cellulaire.nom} </td>   
+                                             <td> {cellulaire.categorie}</td>
+                                             <td> {cellulaire.quantite}</td>
+                                             <td> {cellulaire.prix}</td>
+                                             <td>
+                                                 {/* onClick={ () => this.modifierCellulaire(cellulaire.id)} */}
+                                                 <button className="btn btn-info">Modifier </button>
+                                                 {/* onClick={ () => this.supprimerCellulaire(employee.id)} */}
+                                                 <button style={{marginLeft: "10px"}}  className="btn btn-danger">Supprimer</button>
+                                                
+                                             </td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+
+                 </div>
+
+            </div>
   )
 }
 
