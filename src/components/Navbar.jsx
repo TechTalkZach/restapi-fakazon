ad52371818bca7fa4fed380621da9bd84b628e52
zach <zachalvarezxvi@gmail.com>
2022-11-27 00:31:27 -0500

    Navbar Created, Missing Logo and center Admin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    Navbar,
+    NavItem,
+    NavbarToggler,
+    Collapse,
+    NavLink,
+    Nav,
+    NavbarBrand
+} from 'reactstrap';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+const Header = () => {
 
-export default Navbar
+     // Collapse isOpen State
+     const [isOpen, setIsOpen] = React.useState(false);
+
+     return (
+        <div style={{
+            display: 'block', width: 550, padding: 30
+        }}>
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">ADMINISTRATION</NavbarBrand>
+                <NavbarToggler onClick={() => { setIsOpen(!isOpen) }} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="mr-auto" navbar>
+                       
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div >
+    );
+    }
+
+
+export default Header
