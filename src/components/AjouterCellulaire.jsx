a07ad18a9e7e847bfba43a238be4fe60f95134bb
zach <zachalvarezxvi@gmail.com>
2022-11-28 12:36:08 -0500

    Ajouter & Modifier fini

--- src/components/AjouterCellulaire.jsx
+++ src/components/AjouterCellulaire.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Button, Checkbox, Form } from 'semantic-ui-react';
+// import { Form } from 'react-router-dom';
+import { useNavigate} from 'react-router';
+
+
+
+
+
 
 const AjouterCellulaire = () => {
+
+  const [nom, setNom] = useState('');
+  const [prix, setPrix] = useState('');
+  const [categorie, setCategorie] = useState('');
+  const [quantite, setQuantite] = useState('');
+  const [description, setDescription] = useState('');
+
+  // Navigation apres le POST a l'api
+  const navigate = useNavigate();
+
+
+  // Fonction qui envoie les informations au API
+  const postData = () => {
+    console.log(nom);
+    console.log(prix);
+    console.log(categorie);
+    console.log(description);
+
+  }
+
+// A bien configurer , le History Push nous envoye a la bonne page apres le changement a l'API
+//   const postData = () => {
+//     axios.post(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData`, {
+//         nom,
+//         prix,
+//         categoire,
+//         description
+//     }).then(() => {
+//  navigate('/');
+//})
+// }
+
   return (
-    <div>AjouterCellulaire</div>
+    <div>
+      <h1 id="titre">Ajouter Cellulaire</h1>
+      <Form className='create-form'>
+      <Form.Field>
+        <label>Nom</label>
+        <input placeholder='Nom'  onChange={(e) => setNom(e.target.value)} />
+      </Form.Field>
+      <Form.Field>
+        <label>Prix</label>
+        <input placeholder='Prix' onChange={(e) => setPrix(e.target.value)} />
+      </Form.Field>
+      <Form.Field>
+        <label>Catégorie</label>
+        <input placeholder='Catégorie' onChange={(e) => setCategorie(e.target.value)} />
+      </Form.Field>
+      <Form.Field>
+        <label >Description</label>
+        <input id="description" placeholder='Description' onChange={(e) => setDescription(e.target.value)} />
+      </Form.Field>
+      
+      <Button onClick={postData} type='submit'>Ajouter</Button>
+    </Form>
+    </div>
+    
   )
 }
 
 export default AjouterCellulaire
+
+
+// var QtyPicker = require('react-quantity-picker');
+// var value = 1 ;
+// <QtyPicker
+// value = { value }
+// onChange = {function (newValue) { value = newValue } } />
